e9eaecbcebab4439b7cba72e1df59dad832b270d
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-05-22 13:11:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage />}
+]);
+
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
